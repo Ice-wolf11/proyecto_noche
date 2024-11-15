9d08d85980553ff9c861c9ea9ef172f7541dc0a2
Andrew <andrew@gmail.com>
2024-11-14 19:45:36 -0500

    hola

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,6 +43,12 @@
                                 >
                                 tipos de asistencia
                                 </a>
+                                <a
+                                    href="{{route('attendance-report')}}"
+                                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                >
+                                reportes
+                                </a>
 
                         </nav>
                     </header>
